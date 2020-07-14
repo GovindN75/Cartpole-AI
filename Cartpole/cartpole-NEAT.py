29e6d52c50d5a210d28f2eecae01bc619608b752
GovindN75 <63858322+GovindN75@users.noreply.github.com>
2020-07-14 12:26:26 -0400

    Update cartpole-NEAT.py

--- Cartpole/cartpole-NEAT.py
+++ Cartpole/cartpole-NEAT.py
@@ -75,9 +75,7 @@ def test_final(final_network):
 
 
 if __name__ == '__main__':
-    # Determine path to configuration file. This path manipulation is
-    # here so that the script will run successfully regardless of the
-    # current working directory.
+
     config_path = "path to the config-feedforward.txt file"
     run(config_path)
     
